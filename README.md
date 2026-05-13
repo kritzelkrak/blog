8bf3ba0f6f191d7384c9f6d9dbef683b69d9ea96
kritzelkrak <kritzel@minizou.dev>
2026-05-13 00:13:20 -0400

    Add: basic blog example

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to build
+```bash
+nix develop
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use comrak::{markdown_to_html, Options};
+use std::fs;
+
 fn main() {
-    println!("Hello world!");
+    let template = fs::read_to_string("templates/post.html").unwrap();
+    let post = fs::read_to_string("site/first.md").unwrap();
+
+    let content = markdown_to_html(&post, &Options::default());
+    let page = template
+        .replace("{{title}}", "temp hardcoded title")
+        .replace("{{content}}", &content);
+
+    fs::create_dir_all("build").unwrap();
+    fs::write("build/index.html", page).unwrap();
 }
