912846f8d8303c979bc00f26b368ab3f8d6c4130
kritzelkrak <kritzel@minizou.dev>
2026-05-13 00:39:56 -0400

    Add: Parse frontmatter with toml/serde

--- site/first.md
+++ site/first.md
@@ -1,3 +1,7 @@
++++
+title = "my post"
++++
+
 I was feeling insecure about not understanding
 [1] rust, [2] nix, and [3] building websites, so
 here is iteration 4(?) of my blog!
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 use comrak::{markdown_to_html, Options};
+use serde::Deserialize;
 use std::fs;
+
+#[derive(Debug, Deserialize)]
+struct FrontMatter {
+    title: String
+}
+
+/* Parses a post into its frontmatter and content. */
+fn parse(raw: &str) -> (FrontMatter, String) {
+    let mut parts = raw.splitn(3, "+++");        // ["", "\ntitle", "\ncontent"]
+    parts.next();                               // skip first ""
+
+    let fm_raw = parts.next().expect("Missing frontmatter!");
+    let content = parts.next().expect("Missing content!");
+
+    let fm: FrontMatter = toml::from_str(fm_raw)
+        .expect("Invalid frontmatter!");
+
+        (fm, content.to_string())
+}
 
 fn main() {
     let template = fs::read_to_string("templates/post.html").unwrap();
     let post = fs::read_to_string("site/first.md").unwrap();
 
-    let content = markdown_to_html(&post, &Options::default());
+    let (fm, content) = parse(&post);
+    let html = markdown_to_html(&content, &Options::default());
+    
     let page = template
-        .replace("{{title}}", "temp hardcoded title")
-        .replace("{{content}}", &content);
+        .replace("{{title}}", &fm.title)
+        .replace("{{content}}", &html);
 
     fs::create_dir_all("build").unwrap();
     fs::write("build/index.html", page).unwrap();
